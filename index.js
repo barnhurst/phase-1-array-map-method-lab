e91a9262378da795cb688d509db4688bd201d341
Christopher <c.barnhurst@live.com>
2021-09-09 21:42:22 -0500

    commit tree b77e9e31cad8a56ab0b725cb915f4bdb44049f8c

--- index.js
+++ index.js
@@ -11,6 +11,61 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () =>  {
+  return tutorials.map(title => title.split(' ').map(word => 
+    word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
+  }
+// const titleCased = array => {
+//     let newArray = array.map(element => {
+//       debugger
+//       const words = element.split(" ");
+  
+//       words.map(word => {
+//       
+//         return word[0].toUpperCase() + word.substring(1).toLowerCase;
+//       
+//       }).join(" ")
+//      
+//     })
+//     return newArray;
+//   }
+
+// const titleCased = tutorials => {
+//   tutorials.map(function (element) {
+//     const words = element.split(" ");
+//     words.map((word) => {
+//       return word[0].toUpperCase() + word.substring(1);
+//     }).join(" "); 
+//   })
+// }
+
+// const titleCased = (tutorials) => {
+//   return tutorials.map(tutorials.split(' ').map(charAt(0).toUpperCase() + tutorials.substring(1)).join(' ')
+//   ) 
+
+// }
+
+// const titleCased = tutorials => {
+//   tutorials = el => 
+//     el.map(word => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase)
+    
+//   return tutorials;
+// } 
+
+// const titleCased = () => {
+//   const word = tutorials.split(' ') 
+//   word.map(first => first[0].toUpperCase())
+//   word.join(' ')
+//     return tutorials
+// } 
+
+// const titleCased = 
+//   tutorials.map(titles => titles[0].toUpperCase())
+
+// const titleCased = {tutorials.map(titles => titles[0].toUpperCase())}   
+//   return tutorials
+
+
+// const titleCased = () => {
+//   return tutorials
+// }
